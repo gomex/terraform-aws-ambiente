9b3de6f0d3e310b95e02d8664f045774db1510f8
Rafael Gomes <rgomes@dnsfilter.com>
2026-07-22 16:21:01 -0300

    fix: corrigindo o output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-# output "instance_ip_web" {
-#   description = "IP privado da maquina virtual"
-#   value       = aws_instance.this.private_ip
-# }
+output "instance_ip_web" {
+  description = "IP privado da maquina virtual"
+  value       = { for key, instancia in aws_instance.this : key => instancia.public_ip }
+}
