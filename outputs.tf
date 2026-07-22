d444f5c20329366b57ded1a7b38e46eb152f618e
Rafael Gomes <rgomes@dnsfilter.com>
2026-07-22 15:50:34 -0300

    fix: forçando o erro do output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-# output "instance_ip_web" {
-#   description = "IP privado da maquina virtual"
-#   value       = aws_instance.web.private_ip
-# }
+output "instance_ip_web" {
+  description = "IP privado da maquina virtual"
+  value       = aws_instance.this.private_ip
+}
